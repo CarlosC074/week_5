4b14927e7be25f37707242801deb6bd77a567553
CarlosC074 <carlos.castano74@gmail.com>
2021-08-10 17:48:01 -0700

    All time blocks have been added. Extracted the current hour using moment.js. Each text areas attributed to an hour of the day has been alloted an hour value to compare to the current hour. TODO: iterate over all time blocks to modify the text area's class according to comparisons to areas' hour value to the current hour which has been converted into a number value. TODO: have save button iterate over all text area elements to store their values and display them when the page reloads.

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,34 @@
-var timeBar = document.getElementById("12:00AM");
-var timeSave = document.getElementById("12:00AMSave");
+var allRows = document.getElementsByClassName("timeStamp");
+var currentHour = moment().hour();
+//this array will carry objects. In each one, they will carry an assigned div element(the textarea from each row),
+// and a number to represent an hour of the day to be compared to the current hour
+var timeBlocks= ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24'];
+
+timeAllot();
+
+//each timeBlock will tie an hour value to each text block. this is to allow comparisons between the alloted hour and the current hour, then 
+//modify the element's class accordingly.
+function timeAllot() {
+    for(var i = 0; i < timeBlocks.length; i++) {
+        timeBlocks[i] = {
+            element: allRows[i],
+            hour: i+1,
+        }
+    }
+}
+
+//This function will change the text block's color via class toggling based on the 
+//result of comparing the element's asigned hour to the current hour
+
+
+
+
+
+
+/*init();
 
 function saveData() {
-    data2Save = timeBar.innerHTML;
+    data2Save = timeBar.value;
 
     localStorage.setItem("data2Save", JSON.stringify(data2Save));
 }
@@ -11,7 +37,7 @@ function returnSavedData() {
     var savedData = JSON.parse(localStorage.getItem("data2Save"));
 
     if (savedData !== null) {
-        document.getElementById("12:00AM").innerHTML = savedData;
+        document.getElementById("12:00AM").value = savedData;
     }
     else {
         return;
@@ -25,6 +51,5 @@ function saveBtn() {
 
 function init()  {
     returnSavedData();
-}
+}*/
 
-init();
